2dc9216b9ebc5c1f65b16d157d1270fbda2cd5e7
Hunter <98210343+Hunter1725@users.noreply.github.com>
2023-10-07 10:43:32 +0700

    Add navigation drawer, main layout

--- app/src/main/java/com/example/hikemate/MainActivity.java
+++ app/src/main/java/com/example/hikemate/MainActivity.java
@@ -1,23 +1,63 @@
 package com.example.hikemate;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
 
 import com.example.hikemate.Database.HikeDatabase;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private MaterialToolbar toolbar;
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        initListener();
+    }
+
+    private void initListener() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
+    }
+
+    private void initView() {
+        drawer = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+        bottomNavigationView = findViewById(R.id.bottomNavView);
     }
 
     @Override
